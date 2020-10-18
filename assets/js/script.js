1ef659a01cdcbf3ce7be5575d01a30b1f220a4c9
Casey <caseyderiso@gmail.com>
2020-10-17 20:01:56 -0500

    added real questions, removed .vscode folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,276 +1,286 @@
 // declare global variables
-const mainEl = document.querySelector('main');
-const timeEl = document.getElementById('time');
+const mainEl = document.querySelector("main");
+const timeEl = document.getElementById("time");
 let timeRemaining = NaN;
 let currentQuestion = {};
 let score = 0;
 let highScore = 0;
 
 const q1 = {
-    number: 1,
-    question: "i am a question",
-    answers: ["am i correct?", "or am i correct?", "its probably this one", "or maybe this one"],
-    // index number of correct answer
-    correct: 2
+  number: 1,
+  question: "What is the correct HTML for making a radio button?",
+  answers: [
+    "<radio>",
+    "<radiobutton>",
+    '<input type="radio">',
+    '<input type="radiobutton">',
+  ],
+  // index number of correct answer
+  correct: 2,
 };
 
 const q2 = {
-    number: 2,
-    question: "i am a second question",
-    answers: ["right", "wrong", "wrong", "wrong"],
-    // index number of correct answer
-    correct: 0
+  number: 2,
+  question:
+    'After you initialize a new Git repository and create a file named "developer-quiz.html", which of the following commands will not work if issued?',
+  answers: [
+    "git add developer-quiz.html",
+    "git status",
+    "git add .",
+    'git commit -m "developer quiz web file added"',
+  ],
+  // index number of correct answer
+  correct: 3,
 };
 
 const q3 = {
-    number: 3,
-    question: "i am a third question",
-    answers: ["wrong", "right", "wrong", "wrong"],
-    // index number of correct answer
-    correct: 1
+  number: 3,
+  question: "What is an example of a pseudo-element?",
+  answers: ["::before", "::after", "::first-letter", "All of the above"],
+  // index number of correct answer
+  correct: 3,
 };
+
+const q4 = {};
 // get reference to each question object for input as function parameters
 const questionRefs = [q1, q2, q3];
 
 // create welcome element
-const displayWelcome = function() {
-    // reset the score
-    score = 0;
-    // create welcome elements
-    welcomeEl = document.createElement("div");
-    welcomeTitle = document.createElement("h1");
-    welcomeSubtext = document.createElement("p")
-    buttonEl = document.createElement("div")
-    startButton = document.createElement("button")
-    // give elements attributes and text content
-    welcomeEl.setAttribute("id", "welcome");
-    welcomeEl.setAttribute("class", "big-div")
-    welcomeTitle.setAttribute("class", "element-title");
-    welcomeTitle.textContent = "Welcome to the Web Developer Quiz!"
-    welcomeSubtext.textContent = "Try and and answer the following developer-related question within the time limit. keep in mind that incorrect answers will penalize your score/time by ten seconds!"
-    startButton.textContent = "Start Quiz"
-    startButton.setAttribute("id", "start-quiz")
-    startButton.setAttribute("class", "button")
-    // append the elements to the welcomeEL
-    buttonEl.appendChild(startButton);
-    welcomeEl.appendChild(welcomeTitle);
-    welcomeEl.appendChild(welcomeSubtext);
-    welcomeEl.appendChild(buttonEl);
-    // append welcomeEl to the screen
-    mainEl.appendChild(welcomeEl);
-    
+const displayWelcome = function () {
+  // reset the score
+  score = 0;
+  // create welcome elements
+  welcomeEl = document.createElement("div");
+  welcomeTitle = document.createElement("h1");
+  welcomeSubtext = document.createElement("p");
+  buttonEl = document.createElement("div");
+  startButton = document.createElement("button");
+  // give elements attributes and text content
+  welcomeEl.setAttribute("id", "welcome");
+  welcomeEl.setAttribute("class", "big-div");
+  welcomeTitle.setAttribute("class", "element-title");
+  welcomeTitle.textContent = "Welcome to the Web Developer Quiz!";
+  welcomeSubtext.textContent =
+    "Try and and answer the following developer-related question within the time limit. keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+  startButton.textContent = "Start Quiz";
+  startButton.setAttribute("id", "start-quiz");
+  startButton.setAttribute("class", "button");
+  // append the elements to the welcomeEL
+  buttonEl.appendChild(startButton);
+  welcomeEl.appendChild(welcomeTitle);
+  welcomeEl.appendChild(welcomeSubtext);
+  welcomeEl.appendChild(buttonEl);
+  // append welcomeEl to the screen
+  mainEl.appendChild(welcomeEl);
 };
 
 // create a new question element
-const displayQuestion = function(obj) {
-    // create question elements
-    questionEl = document.createElement("div");
-    questionTextEl = document.createElement("h1");
-    answerOlEl = document.createElement("ol");
-    // give elements attributes and text content from obj
-    questionEl.setAttribute("class", "big-div");
-    questionTextEl.setAttribute("class", "question-text");
-    answerOlEl.setAttribute("class", "button-list");
-    questionEl.setAttribute("data-question-id", obj.number);
-    questionTextEl.textContent = obj.question;
-    // create answer elements, attributes and text content in a loop of 4
-    for (i=0; i<4; i++) {
-        answerEl = document.createElement("li");
-        answerButton = document.createElement("button");
+const displayQuestion = function (obj) {
+  // create question elements
+  questionEl = document.createElement("div");
+  questionTextEl = document.createElement("h1");
+  answerOlEl = document.createElement("ol");
+  // give elements attributes and text content from obj
+  questionEl.setAttribute("class", "big-div");
+  questionTextEl.setAttribute("class", "question-text");
+  answerOlEl.setAttribute("class", "button-list");
+  questionEl.setAttribute("data-question-id", obj.number);
+  questionTextEl.textContent = obj.question;
+  // create answer elements, attributes and text content in a loop of 4
+  for (i = 0; i < 4; i++) {
+    answerEl = document.createElement("li");
+    answerButton = document.createElement("button");
 
-        answerButton.setAttribute("class", "button");
-        answerButton.setAttribute("data-answer-id", i);
-        answerButton.textContent = obj.answers[i];
+    answerButton.setAttribute("class", "button");
+    answerButton.setAttribute("data-answer-id", i);
+    answerButton.textContent = obj.answers[i];
 
-        answerEl.appendChild(answerButton);
-        answerOlEl.appendChild(answerEl);
-    };
-    // append question and answers to question element
-    questionEl.appendChild(questionTextEl);
-    questionEl.appendChild(answerOlEl);
-    // append question element to the screen
-    mainEl.appendChild(questionEl);
+    answerEl.appendChild(answerButton);
+    answerOlEl.appendChild(answerEl);
+  }
+  // append question and answers to question element
+  questionEl.appendChild(questionTextEl);
+  questionEl.appendChild(answerOlEl);
+  // append question element to the screen
+  mainEl.appendChild(questionEl);
 };
 
 // display end screen after last question
-const displayEndScreen = function() {
-    // create elements
-    endEl = document.createElement("div");
-    h1El = document.createElement("h1");
-    formEl = document.createElement("form");
-    nameInput = document.createElement("input");
-    formButton = document.createElement("button");
-    buttonEl = document.createElement("div");
-    startButton = document.createElement("button");
-    // set attributes and content
-    h1El.textContent = "Game over, man.";
-    endEl.setAttribute("class", "big-div");
-    formEl.setAttribute("id", "score-form");
-    formButton.setAttribute("id", "score-submit-button")
-    formButton.textContent = "Submit High Score"
-    nameInput.setAttribute("id", "name-input")
-    nameInput.setAttribute("type", "text");
-    nameInput.setAttribute("placeholder", "enter your initials");
-    startButton.setAttribute("id", "start-quiz");
-    startButton.setAttribute("class", "button");
-    startButton.textContent = "Try Again"
-    // append
-    formEl.appendChild(nameInput);
-    formEl.appendChild(formButton)
-    buttonEl.appendChild(startButton);
-    endEl.appendChild(h1El);
-    endEl.appendChild(formEl);
-    endEl.appendChild(buttonEl);
+const displayEndScreen = function () {
+  // create elements
+  endEl = document.createElement("div");
+  h1El = document.createElement("h1");
+  formEl = document.createElement("form");
+  nameInput = document.createElement("input");
+  formButton = document.createElement("button");
+  buttonEl = document.createElement("div");
+  startButton = document.createElement("button");
+  // set attributes and content
+  h1El.textContent = "Game over, man.";
+  endEl.setAttribute("class", "big-div");
+  formEl.setAttribute("id", "score-form");
+  formButton.setAttribute("id", "score-submit-button");
+  formButton.textContent = "Submit High Score";
+  nameInput.setAttribute("id", "name-input");
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("placeholder", "enter your initials");
+  startButton.setAttribute("id", "start-quiz");
+  startButton.setAttribute("class", "button");
+  startButton.textContent = "Try Again";
+  // append
+  formEl.appendChild(nameInput);
+  formEl.appendChild(formButton);
+  buttonEl.appendChild(startButton);
+  endEl.appendChild(h1El);
+  endEl.appendChild(formEl);
+  endEl.appendChild(buttonEl);
 
-    mainEl.appendChild(endEl);
-    // store the high score
-    formRef = document.querySelector('#score-form');
+  mainEl.appendChild(endEl);
+  // store the high score
+  formRef = document.querySelector("#score-form");
 
-    formRef.addEventListener("submit", function (event) {
-        event.preventDefault();
-        nameInput = event.srcElement[0].value;
+  formRef.addEventListener("submit", function (event) {
+    event.preventDefault();
+    nameInput = event.srcElement[0].value;
 
-        localStorage.setItem("initials", nameInput);
-        localStorage.setItem("high-score", score);
+    localStorage.setItem("initials", nameInput);
+    localStorage.setItem("high-score", score);
 
-        formEl.reset();
-    })
-    
-    // .addEventListener("sumbit", function (event) {
-    //     event.preventDefault();
-    // });
-    timeRemaining = 0;
-}
+    formEl.reset();
+  });
+  timeRemaining = 0;
+};
 
-const displayHighScore = function() {
-    timeRemaining = 0;
-    score = 0;
+const displayHighScore = function () {
+  timeRemaining = 0;
+  score = 0;
 
-    scoreEl = document.createElement("div");
-    h1El = document.createElement("h1");
-    scoreDisplayEl = document.createElement("h2");
-    buttonEl = document.createElement("div")
-    startButton = document.createElement("button")
+  scoreEl = document.createElement("div");
+  h1El = document.createElement("h1");
+  scoreDisplayEl = document.createElement("h2");
+  buttonEl = document.createElement("div");
+  startButton = document.createElement("button");
 
+  scoreEl.setAttribute("class", "big-div");
+  h1El.textContent = "High Score: ";
+  startButton.textContent = "Start Quiz";
+  startButton.setAttribute("id", "start-quiz");
+  startButton.setAttribute("class", "button");
 
-    scoreEl.setAttribute("class", "big-div");
-    h1El.textContent = "High Score: "
-    startButton.textContent = "Start Quiz"
-    startButton.setAttribute("id", "start-quiz")
-    startButton.setAttribute("class", "button")
+  if (localStorage.getItem("high-score") == null) {
+    scoreDisplayEl.textContent = "There is no High Score set!";
+  } else {
+    scoreDisplayEl.textContent =
+      localStorage.getItem("high-score") +
+      " " +
+      localStorage.getItem("initials");
+  }
+  buttonEl.appendChild(startButton);
+  scoreEl.appendChild(h1El);
+  scoreEl.appendChild(scoreDisplayEl);
+  scoreEl.appendChild(buttonEl);
 
-    if (localStorage.getItem("high-score") == null) {
-        scoreDisplayEl.textContent = "There is no High Score set!"
-    } 
-    else {
-    scoreDisplayEl.textContent = localStorage.getItem("high-score") + " " + localStorage.getItem("initials");
-    }
-    buttonEl.appendChild(startButton);
-    scoreEl.appendChild(h1El);
-    scoreEl.appendChild(scoreDisplayEl);
-    scoreEl.appendChild(buttonEl);
+  mainEl.appendChild(scoreEl);
+};
 
-    mainEl.appendChild(scoreEl);
-}
-
-
-
-// clear screen and replace with next div
-const clearAndReplace = function(nextEl) {
-    const displayEl = document.querySelector(".big-div")
-    mainEl.removeChild(displayEl);
-    // see if we are replacing current element with high score screen
-    if (nextEl === "highScore") {
-        displayHighScore()
-    } 
-    // check if question number for next El is greater than the list of questions
-    else if (nextEl > questionRefs.length - 1) {
-        displayEndScreen()
-    } 
-    // display next question
-    else {
-    input = questionRefs[nextEl]
+// clear screen and replace with next div input is the next question number, or "highScore"
+const clearAndReplace = function (nextEl) {
+  const displayEl = document.querySelector(".big-div");
+  mainEl.removeChild(displayEl);
+  // see if we are replacing current element with high score screen
+  if (nextEl === "highScore") {
+    displayHighScore();
+  }
+  // check if question number for next El is greater than the list of questions
+  else if (nextEl > questionRefs.length - 1) {
+    displayEndScreen();
+  }
+  // display next question
+  else {
+    input = questionRefs[nextEl];
     displayQuestion(input);
-    };
+  }
 };
 
 // start count down
-const countDown = function() {
-    let timeInterval = setInterval(function() {
-        if (timeRemaining < 1) {
-            timeRemaining = 0;
-            clearInterval(timeInterval);
-            clearAndReplace(questionRefs.length)
-        };
+const countDown = function () {
+  let timeInterval = setInterval(function () {
+    if (timeRemaining < 1) {
+      // set to zero if a wrong answer is selected with less than 10 seconds left
+      timeRemaining = 0;
+      // stop the timer
+      clearInterval(timeInterval);
+      // send to end screen by calling a question number that is larger than the list of questions
+      clearAndReplace(questionRefs.length);
+    }
     timeEl.textContent = timeRemaining;
     timeRemaining--;
-    }, 1000);
+  }, 1000);
 };
 
 // subtract 10 seconds from clock
-const timePenalty = function() {
-    if (timeRemaining > 0) {
-        timeRemaining = timeRemaining - 10;
-    }
+const timePenalty = function () {
+  if (timeRemaining > 0) {
+    timeRemaining = timeRemaining - 10;
+  }
 };
 
 displayWelcome();
 
-// set up button event listener 
+// set up button event listener
 
-mainEl.addEventListener("click", function(event) {
-    // get reference to what element was clicked
-    const mouse = event.target;
-    // get reference to the main element to clear it. 
-    let parent = event.path.length - 6;
-    // see if the user clicked a button
-    if (mouse.className === "button") {
-
-        // if that button is the start quiz button
-        if (mouse.id === "start-quiz"){
-            // reset timer 
-            timeRemaining = 60;
-            // start the timer
-            countDown();
-            // clear screen and display the questions
-            clearAndReplace(0);
-        } 
-        // if that button is an answer button
-        else if (mouse.hasAttribute("data-answer-id")) {
-            // get reference to which answer was clicked
-            selectedAnswer = mouse.getAttribute("data-answer-id");
-            // get reference to the current question ID (1 index)
-            let currentQuestionId = event.path[parent].getAttribute("data-question-id");
-            // use question ID to get reference to question object
-            let currentQuestionObj = questionRefs[currentQuestionId - 1]
-            // get reference to the correct answer
-            correctAnswer = currentQuestionObj.correct;
-            // if answer is correct
-            if (selectedAnswer == correctAnswer) {
-                console.log("that's it")
-                // increment the score
-                score++;
-                clearAndReplace(currentQuestionId);
-            } 
-            // if answer is incorrect, remove time
-            else {
-                console.log("nope");
-                timePenalty();
-            };
-        };
+mainEl.addEventListener("click", function (event) {
+  // get reference to what element was clicked
+  const mouse = event.target;
+  // get reference to the main element to clear it.
+  let parent = event.path.length - 6;
+  // see if the user clicked a button
+  if (mouse.className === "button") {
+    // if that button is the start quiz button
+    if (mouse.id === "start-quiz") {
+      // reset timer
+      timeRemaining = 60;
+      // start the timer
+      countDown();
+      // clear screen and display the questions
+      clearAndReplace(0);
     }
-    // if the user clicked the high scores button
-    else if (mouse.id === "high-score") {
-        if (timeRemaining > 1) {
-            alert("Please finish your quiz!")
-        }
-        else {
-        clearAndReplace("highScore");
-        }
+    // if that button is an answer button
+    else if (mouse.hasAttribute("data-answer-id")) {
+      // get reference to which answer was clicked
+      selectedAnswer = mouse.getAttribute("data-answer-id");
+      // get reference to the current question ID (1 index)
+      let currentQuestionId = event.path[parent].getAttribute(
+        "data-question-id"
+      );
+      // use question ID to get reference to question object
+      let currentQuestionObj = questionRefs[currentQuestionId - 1];
+      // get reference to the correct answer
+      correctAnswer = currentQuestionObj.correct;
+      // if answer is correct
+      if (selectedAnswer == correctAnswer) {
+        console.log("that's it");
+        // increment the score
+        score++;
+        clearAndReplace(currentQuestionId);
+      }
+      // if answer is incorrect, remove time
+      else {
+        console.log("nope");
+        timePenalty();
+      }
     }
-    // if the user didn't click a button or the high scores
-    else {
-        return;
-    };
+  }
+  // if the user clicked the high scores button
+  else if (mouse.id === "high-score") {
+    if (timeRemaining > 1) {
+      alert("Please finish your quiz!");
+    } else {
+      clearAndReplace("highScore");
+    }
+  }
+  // if the user didn't click a button or the high scores
+  else {
+    return;
+  }
 });
-
